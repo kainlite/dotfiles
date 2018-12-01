bbc6eff6622b00fae5dac39fe228c60642aa2bd8
gabriel.garrido <gabriel.garrido@lavu.com>
2018-12-01 09:52:35 -0300

    Added gnome-terminal back

--- rc.lua
+++ rc.lua
@@ -44,7 +44,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "gnome-terminal"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
