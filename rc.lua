5c2b36a5414a06010054620a5b22ab3794e751f3
kainlite <gabriel.garrido@lavu.com>
2019-01-09 16:15:09 -0300

    Adding flameshot full path

--- rc.lua
+++ rc.lua
@@ -56,7 +56,8 @@ local function run_once(cmd_arr)
     end
 end
 
-run_once({ "urxvtd", "unclutter -root", "flameshot", "megasync", "xscreensaver -nosplash", "xsettingsd" }) -- entries must be separated by commas
+-- entries must be separated by commas
+run_once({ "urxvtd", "unclutter -root", "megasync", "xscreensaver -nosplash", "xsettingsd", "/usr/bin/flameshot" })
 
 -- This function implements the XDG autostart specification
 --[[
