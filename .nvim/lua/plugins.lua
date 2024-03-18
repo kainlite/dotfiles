6e6c89b73204f128f8615520502765811213fdab
kainlite <kainlite@gmail.com>
2024-03-18 13:12:22 -0300

    Update

--- .nvim/lua/plugins.lua
+++ .nvim/lua/plugins.lua
@@ -150,7 +150,17 @@ require("lazy").setup({
   { "onsails/lspkind.nvim" },
 
   -- snips
-  "L3MON4D3/LuaSnip",
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
 
   "JoosepAlviste/nvim-ts-context-commentstring",
 
