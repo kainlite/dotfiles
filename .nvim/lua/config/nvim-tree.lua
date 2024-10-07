842022a183a4a9984f01fb9bd8df641f954a07ca
kainlite <kainlite@gmail.com>
2024-10-07 11:55:20 -0300

    fix: don't hide ignored files

--- .nvim/lua/config/nvim-tree.lua
+++ .nvim/lua/config/nvim-tree.lua
@@ -4,5 +4,13 @@ require("nvim-tree").setup({
     update_root = false,
     update_cwd = true,
   },
+  filters = {
+    dotfiles = false,
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
   sync_root_with_cwd = true,
 })
